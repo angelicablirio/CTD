d714aa430523919fdfe70235508e056912e8670e
Angelica Lirio <85036081+angelicablirio@users.noreply.github.com>
2021-09-28 23:02:14 -0300

    Delete diretório

--- programacao-imperativa/aula15/index.js
+++
@@ -1,7 +0,0 @@
-var Aluno = require('./aluno/aluno.controlador');
-
-var alunoNovo = new Aluno ('Angelica', 'Lirio');
-
-console.log('Nome:', alunoNovo.pegarNome());
-console.log('Sobrenome:', alunoNovo.pegarSobrenome());
-console.log('Nome Completo:', alunoNovo.pegarNomeCompleto());
