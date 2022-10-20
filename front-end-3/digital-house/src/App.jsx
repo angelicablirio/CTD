fc18c6d863f8fadeca753f89669583e018b065ca
Angelica Barbieri Lirio <angelicablirio@gmail.com>
2022-10-19 22:55:30 -0300

    Atividades segunda aula

--- front-end-3/digital-house/src/App.jsx
+++ front-end-3/digital-house/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { SegundaAula } from "./aulas/SegundaAula"
+import { Login } from "./pages/Login"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <h1 className="read-the-docs">
-        Hello world!
-      </h1>
-    </div>
-  )
-}
 
-export default App
+    //<Login />
+    <SegundaAula />
+
+  );
+};
+
+export default App;
---
+++ front-end-3/digital-house/src/pages/Login/index.jsx
@@ -0,0 +1,6 @@
+export function Login() {
+  return(
+    <h1 className="Title">Eu sou o componente de Login!</h1>
+  );
+};
+
